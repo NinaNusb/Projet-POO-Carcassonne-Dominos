0f8dbea7bea88cc3c97dca92831fa0e4a6b50441
mducos <103144484+mducos@users.noreply.github.com>
2022-12-21 16:00:02 +0100

    Update Vue.java

--- Vue.java
+++ Vue.java
@@ -1,7 +1,6 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -19,9 +18,10 @@ public class Vue extends JFrame {
         this.window = new JPanel();
         this.vuePlateau = new JPanel();
         this.panneauChoix = new JPanel();
-        File file = new File("C:/Users/carol/IdeaProjects/Carcassonne V2/src/Image tuile/Base_Game_C2_Tile_A1.jpg");
+        File file = new File("C:/Users/carol/IdeaProjects/Carcassonne V2/src/Image tuile/Base_Game_C2_Tile_A1.jpg"); // TODO mettre une IOException
         BufferedImage path = ImageIO.read(file);
         this.tuilePiochee = new ImagePane(path);
+        this.modele.setTuilePiochee(this.tuilePiochee);
 
         this.setTitle("Carcassonne");
         this.setSize(1200, 800);
@@ -32,10 +32,7 @@ public class Vue extends JFrame {
         for(int i = 0; i < p.getPlateau().size(); i++){
             for(int j = 0; j < p.getPlateau().get(0).size(); j++){
                 JButton btn = new JButton();
-                btn.setSize(new Dimension(Math.min(700/p.getPlateau().get(0).size(), 700/p.getPlateau().size()), Math.min(800/p.getPlateau().get(0).size(),800/p.getPlateau().size())));
-
-                System.out.println(Math.min(800/p.getPlateau().get(0).size(), 800/p.getPlateau().size())); // TODO
-                System.out.println(btn.getWidth() + " " + btn.getHeight()); // TODO
+                btn.setSize(new Dimension(Math.min(800/p.getPlateau().get(0).size(), 800/p.getPlateau().size()), Math.min(800/p.getPlateau().get(0).size(),800/p.getPlateau().size())));
 
                 if(!p.getPlateau().get(i).get(j).estVide()){
                     ImageIcon imageIcon = new ImageIcon(((TuileCarcassonne)p.getPlateau().get(i).get(j).getTuile()).getChemin().getAbsolutePath());
@@ -43,11 +40,7 @@ public class Vue extends JFrame {
                     Image newimg = image.getScaledInstance(btn.getWidth(), btn.getHeight(),  java.awt.Image.SCALE_SMOOTH);
                     imageIcon = new ImageIcon(newimg);
                     btn = new JButton(imageIcon);
-                    System.out.println("IMAGE"); // TODO
-                    System.out.println(btn.getWidth() + " " + btn.getHeight()); // TODO
-                    System.out.println(imageIcon.getIconWidth() + " " + imageIcon.getIconHeight()); // TODO
                     btn.setSize(new Dimension(imageIcon.getIconWidth(), imageIcon.getIconHeight()));
-                    System.out.println("btn " + btn.getWidth() + " " + btn.getHeight()); // TODO
 
                 }
                 this.vuePlateau.add(btn);
@@ -67,7 +60,7 @@ public class Vue extends JFrame {
         tuilePiochee.setPreferredSize(new Dimension(110,110));
         //tuilePiochee.setAlignmentX(Component.CENTER_ALIGNMENT);
         //this.tuilePiochee.setIcon(new ImageIcon("C:/Users/carol/IdeaProjects/Carcassonne V2/src/Image tuile/Base_Game_C2_Tile_A1.jpg"));
-        this.panneauChoix.add(tuilePiochee);
+        //this.panneauChoix.add(tuilePiochee);
         //JLabel ligne2 = new JLabel("test");
         //ligne2.setPreferredSize(new Dimension(this.getWidth()/3, 50));
         //ligne2.setHorizontalAlignment(JLabel.CENTER);
@@ -75,7 +68,6 @@ public class Vue extends JFrame {
         this.panneauChoix.add(tournerButton);
         this.tuilePiochee.setPreferredSize(new Dimension(110,110));
         this.panneauChoix.add(this.tuilePiochee);
-        //this.panneauChoix.add(Box.createVerticalGlue());
 
         this.window.setLayout(new BoxLayout(this.window, BoxLayout.LINE_AXIS));
         this.window.add(panneauChoix);
@@ -83,15 +75,15 @@ public class Vue extends JFrame {
         this.window.add(Box.createHorizontalGlue());
         this.getContentPane().add(window);
 
+        pack();
+        setVisible(true);
+
         tournerButton.addActionListener(event -> {
+            this.panneauChoix.remove(this.tuilePiochee);
             final BufferedImage bi;
-            try {
-                bi = ImageIO.read(new File("C:/Users/carol/Desktop/M1/POO/projet Carcassonne/images des tuiles/Base_Game_C2_Tile_A1.jpg"));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("ici"); // TODO
-            this.panneauChoix.add(new JPanel() {
+            bi = createImage(this.tuilePiochee);
+
+            this.tuilePiochee = new JPanel() {
 
                 @Override
                 public Dimension getPreferredSize() {
@@ -99,21 +91,30 @@ public class Vue extends JFrame {
                 }
 
                 @Override
-                protected void paintComponent(Graphics g) {
+                public void paintComponent(Graphics g) {
                     super.paintComponent(g);
                     Graphics2D g2 = (Graphics2D) g;
                     g2.rotate(Math.PI/2, bi.getWidth() / 2, bi.getHeight() / 2);
                     g2.drawImage(bi, 0, 0, null);
                 }
-            });
-            //this.tuilePiochee.paintComponents();
-            System.out.println(this.tuilePiochee.getClass()); // TODO
+            };
+            this.panneauChoix.add(this.tuilePiochee);
+            controleur.tuileTournee(this.tuilePiochee);
         });
 
-
+    }
+    private BufferedImage createImage(JPanel panel) {
+        int w = panel.getWidth();
+        int h = panel.getHeight();
+        BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = bi.createGraphics();
+        panel.paint(g);
+        return bi;
+    }
+    public void miseAJourTuilePiochee() {
+        this.tuilePiochee = this.modele.tuilePiochee;
         pack();
         setVisible(true);
     }
 
-    // méthode qui met à jour l'image de la tuile piochée à chaque tour
 }
