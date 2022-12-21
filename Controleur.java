77ac987890a650c33e6fcc48f3a07bcb5444a68e
mducos <103144484+mducos@users.noreply.github.com>
2022-12-21 15:00:31 +0100

    Add files via upload

---
+++ Controleur.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Controleur {
+    Modele modele;
+    Vue vue;
+    public Controleur(Modele modele){
+        this.modele = modele;
+    }
+    public void setVue(Vue vue){
+        this.vue = vue;
+    }
+    public void tuileTournee(JPanel tuilePiochee){
+        modele.setTuilePiochee(tuilePiochee);
+        vue.miseAJourTuilePiochee();
+    }
+}
--- Modele.java
+++ Modele.java
@@ -1,2 +1,11 @@
+import javax.swing.*;
+
 public class Modele {
+    JPanel tuilePiochee;
+
+    public Modele(){
+    }
+    public void setTuilePiochee(JPanel tuilePiochee){
+        this.tuilePiochee = tuilePiochee;
+    }
 }
