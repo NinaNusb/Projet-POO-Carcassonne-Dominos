c31454c17b3985a7ba9c440833fd85841c9ce939
mducos <103144484+mducos@users.noreply.github.com>
2022-12-23 17:51:58 +0100

    Update Lieu.java

--- Lieu.java
+++ Lieu.java
@@ -1,29 +1,29 @@
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class Lieu {
-    protected Lieu lieu1;
-    protected Lieu lieu2;
-    protected Lieu lieu3;
-    int numeroUnique;
+    protected int points;
+    protected ArrayList<Lieu> zone;
+    private Joueur possesseurPartisan;
+    private int numeroUnique;
     public static AtomicInteger genID = new AtomicInteger();
 
-    public Lieu(Lieu[] lieux){
-        this.lieu1 = lieux[0];
-        this.lieu2 = lieux[1];
-        this.lieu3 = lieux[2];
-    
+    public Lieu(){}
+
+    public int getNumeroUnique(){
+        return this.numeroUnique;
     }
-    public Lieu getLieu1(){
-        return this.lieu1;
-    }
-    public Lieu getLieu2(){
-        return this.lieu2;
-    }
-    public Lieu getLieu3(){
-        return this.lieu3;
+    public void setNumeroUnique(int x){
+        this.numeroUnique = x;
     }
 
     public void donneIndice(Lieu lieuAdjacent){
         this.numeroUnique = lieuAdjacent.numeroUnique;
+    }
+    public void ajouterPartisan(Joueur joueur){
+        this.possesseurPartisan = joueur;
+    }
+    public void retirerPartisan(){
+        this.possesseurPartisan = null;
     }
 }
