842e48ce409b756faac5d32aea8bf555f9594fac
mducos <103144484+mducos@users.noreply.github.com>
2022-12-23 17:53:37 +0100

    Create PlateauDomino.java

---
+++ PlateauDomino.java
@@ -0,0 +1,8 @@
+public class PlateauDomino extends Plateau{
+
+    public PlateauDomino(SacDomino sac){
+        super(sac);
+        // on pioche la tuile de départ au hasard et on pose au milieu du plateau
+        this.ajouterTuile(this.plateau.get(0).get(0), sac.piocheTuile());
+    }
+}
